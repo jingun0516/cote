7a972f3029e38388b5dde28233dd42acd00f2254
jg <ephigh1397@naver.com>
2024-08-26 14:39:24 +0900

    pm12936 math?

---
+++ cote/pm12936.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+long long fact(long long num) {
+    long long factNum = 1;
+
+    for (int i = 1; i <= num; i++)
+        factNum *= i;
+
+    return factNum;
+}
+
+vector<int> solution(int n, long long k) {
+    vector<int> answer;
+    vector<bool> check(n + 1, false);
+
+    int temp = n;
+    while (answer.size() < temp) {
+        long long next = fact(n - 1);
+
+        for (int i = 1; i <= temp; i++) {
+            if (check[i]) continue;
+
+            if (k <= next) {
+                answer.push_back(i);
+                check[i] = true;
+                break;
+            }
+            else {
+                k -= next;
+            }
+        }
+
+        n--;
+    }
+
+    for (auto a : answer)
+        cout << a << " ";
+
+    return answer;
+}
+
+
+int main()
+{
+    int n = 3;
+    long long k = 6;
+
+    solution(n, k);
+}
